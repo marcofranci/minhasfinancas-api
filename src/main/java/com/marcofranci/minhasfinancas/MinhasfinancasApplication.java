ccd39f01bbeaf620851cf624deb11ae91f79548d
marco <franci1983@hotmail.com>
2020-04-29 16:03:09 -0300

    Concluindo o controller de Lancamentos

--- src/main/java/com/marcofranci/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/marcofranci/minhasfinancas/MinhasfinancasApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinhasfinancasApplication {
-
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MinhasfinancasApplication.class, args);
 		System.out.println("Aplicação iniciada.");
 	}
